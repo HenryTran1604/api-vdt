dc0064c7e09d3e6a27898aff50b7f7126968dc0a
HenryTran1604 <quanghuy1604nd@gmail.com>
2024-05-24 14:46:11 +0700

    Update: api url

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-
+import os
 
 app = Flask(__name__)
 CORS(app)
-client = MongoClient('mongodb://localhost:27017')
+client = MongoClient(os.environ.get('MONGO_URL', 'mongodb://localhost:27017'))
 db = client['midterm']
